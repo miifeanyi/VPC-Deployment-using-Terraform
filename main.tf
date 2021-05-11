d19ddf8dbb6f809c6f27303c8874e8ab7014a7b1
Michael Ifeanyi <ifeanyi.chemezie@gmail.com>
2021-05-11 06:43:08 -0600

    removed main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-#this is just dummy file used for main branch. main code is in vpc.tf file
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     "Name" = "main"
-#   }
-# }
